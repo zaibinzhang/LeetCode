79d68898a4054813aa24b338ed0cf579c3543ef4
zaibinzhang <zaibinzhang@qq.com>
2016-09-19 15:27:35 +0800

    完成Simplify Path

---
+++ LeetCode/Algorithm/SimplifyPath.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Algorithm
+{
+    public partial class Solution
+    {
+        /// <summary>
+        /// url:https://leetcode.com/problems/simplify-path/
+        /// Given an absolute path for a file (Unix-style), simplify it.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public string SimplifyPath(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                return "/";
+            }
+            string[] paths = path.Split('/');
+            if (paths.Length < 2)
+            {
+                return "/";
+            }
+            Stack<string> stack = new Stack<string>();
+            for (int i = 0; i < paths.Length; i++)
+            {
+                if (paths[i] == "" || paths[i] == ".")
+                {
+                    continue;
+                }
+
+                if (paths[i] == "..")
+                {
+                    if (stack.Count != 0)
+                    {
+                        stack.Pop();
+                    }
+                    continue;
+                }
+
+                stack.Push(paths[i]);
+            }
+
+            string ret = "";
+            while (stack.Count != 0)
+            {
+                ret = $"/{stack.Pop()}{ret}";
+            }
+            return ret == "" ? "/" : ret;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LeetCode.Algorithm;
 
 namespace LeetCode
 {
@@ -10,6 +11,11 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
+            Solution sol = new Solution();
+            Console.WriteLine(sol.SimplifyPath("/home/foo/.ssh/../.ssh2/authorized_keys/"));
+
+
+            Console.ReadKey();
         }
     }
 }
