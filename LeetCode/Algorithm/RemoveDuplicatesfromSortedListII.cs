729bc4d2635d6a6410dd1a5a9e5f3b23b02f5f4a
zaibinzhang <zaibinzhang@qq.com>
2016-09-28 13:39:56 +0800

    完成Remove Duplicates from Sorted List II

---
+++ LeetCode/Algorithm/RemoveDuplicatesfromSortedListII.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.Model;
+
+namespace LeetCode.Algorithm
+{
+    public partial class Solution
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head==null)
+            {
+                return null;
+            }
+            int cur = head.val;
+            bool b = false;
+            while (head.next != null)
+            {
+                if (head.next.val == cur)
+                {
+                    head.next = head.next.next;
+                    b = true;
+                }
+                else
+                {
+                    head.next = DeleteDuplicates(head.next);
+                    break;
+                }
+            }
+            if (b)
+            {
+                return head.next;
+            }
+            else
+            {
+                return head;
+            }
+        }
+    }
+}
