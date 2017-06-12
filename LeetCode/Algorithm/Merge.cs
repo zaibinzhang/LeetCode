3761b262f54c191d6a483082c3c5a683cceb9578
zaibinzhang <zaibinzhang@qq.com>
2017-06-12 16:40:02 +0800

    添加题目

---
+++ LeetCode/Algorithm/Merge.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using LeetCode.Model;
+
+namespace LeetCode.Algorithm
+{
+    public partial class Solution
+    {
+        public IList<Interval> Merge(IList<Interval> intervals)
+        {
+
+        }
+    }
+}
