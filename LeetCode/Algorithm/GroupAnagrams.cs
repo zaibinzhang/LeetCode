fe87b45857d099596397990ab8fe88dc4f1ed024
zaibinzhang <zaibinzhang@qq.com>
2017-06-12 16:34:58 +0800

    完成Group Anagrams ，论效率，Dictionary效率高很多

---
+++ LeetCode/Algorithm/GroupAnagrams.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Algorithm
+{
+    public partial class Solution
+    {
+        //49. Group Anagrams
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            Dictionary<string, IList<string>> dic = new Dictionary<string, IList<string>>();
+            foreach (string s in strs)
+            {
+                var cs = s.ToCharArray();
+                Array.Sort(cs);
+                string tempcs = new string(cs);
+                if (dic.ContainsKey(tempcs))
+                {
+                    //list[temp.IndexOf(tempcs)].Add(s);
+                    dic[tempcs].Add(s);
+                }
+                else
+                {
+                    dic.Add(tempcs, new List<string>() { s });
+                }
+            }
+            return new List<IList<string>>(dic.Values);
+        }
+    }
+}
