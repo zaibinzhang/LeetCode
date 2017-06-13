ee57aef24b4888b59a6641a22f8b7b7120fe13f3
zaibinzhang <zaibinzhang@qq.com>
2017-06-13 11:22:54 +0800

    新增测试框架

---
+++ LeetCodeTests/Algorithm/NumDecodingsTests.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Algorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Algorithm
+{
+    [TestClass()]
+    public partial class SolutionTests
+    {
+        [TestMethod()]
+        public void NumDecodingsTest()
+        {
+            Solution sol = new Solution();
+            Assert.AreEqual(sol.NumDecodings("12"), 2);
+        }
+    }
+}
