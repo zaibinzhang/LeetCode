965879331c5a50bd4fd0aaa9d4500e34990b50a1
zaibinzhang <zaibinzhang@qq.com>
2017-06-13 14:21:11 +0800

    难难难

--- LeetCode/Algorithm/ReverseBetween.cs
+++ LeetCode/Algorithm/ReverseBetween.cs
@@ -1,11 +1,34 @@
-﻿using LeetCode.Model;
+﻿using System.Collections.Generic;
+using LeetCode.Model;
 
 namespace LeetCode.Algorithm
 {
     public partial class Solution
     {
+        //92. Reverse Linked List II
         public ListNode ReverseBetween(ListNode head, int m, int n)
         {
+            ListNode mhead, thead = head, nhead;
+            int i;
+            if (m != 1)
+            {
+                i = 1;
+                while (i < m)
+                {
+                    i++;
+                    thead = thead.next;
+                }
+                mhead = thead;
+            }
+            i = 1;
+            thead = head;
+            while (i < n)
+            {
+                i++;
+                thead = thead.next;
+            }
+            nhead = thead;
+
             return null;
         }
     }
