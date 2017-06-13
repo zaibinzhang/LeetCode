489ebe93517e594d37bfb9898d3b59768f679c73
zaibinzhang <zaibinzhang@qq.com>
2017-06-13 13:33:45 +0800

    迷之1秒

--- LeetCode/Algorithm/NumDecodings.cs
+++ LeetCode/Algorithm/NumDecodings.cs
@@ -11,25 +11,38 @@ namespace LeetCode.Algorithm
                 return 0;
             }
 
-            if (s.Length == 1)
+            if (s.Length > 2)
             {
-                if (s[0] == '0')
+                if (s[0] == '1' || s[0] == '2' && s[1] <= '6')
+                {
+                    if (s[1] == '0')
+                    {
+                        return NumDecodings(s.Substring(2));
+                    }
+                    return NumDecodings(s.Substring(1)) + NumDecodings(s.Substring(2));
+                }
+
+                return NumDecodings(s.Substring(1));
+
+            }
+
+            if (s.Length == 2)
+            {
+                if ((s[0] == '1' || s[0] == '2' && s[1] <= '6') && s[1] != '0')
+                {
+                    return 2;
+                }
+                else if (s[0] >= '3' && s[1] == '0')
                 {
                     return 0;
                 }
-                return 1;
+                else
+                {
+                    return 1;
+                }
             }
 
-            int num = System.Convert.ToInt32(s.Substring(0, 2));
-            if (num <= 26)
-            {
-                var i = s.Length == 2 ? 1 : NumDecodings(s.Substring(2));
-                return NumDecodings(s.Substring(1)) + i;
-            }
-            else
-            {
-                return NumDecodings(s.Substring(1));
-            }
+            return 1;
         }
     }
 }
--- LeetCodeTests/Algorithm/NumDecodingsTests.cs
+++ LeetCodeTests/Algorithm/NumDecodingsTests.cs
@@ -7,10 +7,52 @@ namespace LeetCodeTests.Algorithm
     public partial class SolutionTests
     {
         [TestMethod()]
-        public void NumDecodingsTest()
+        public void NumDecodingsTest1()
         {
             Solution sol = new Solution();
             Assert.AreEqual(sol.NumDecodings("12"), 2);
+        }
+
+        [TestMethod()]
+        public void NumDecodingsTest2()
+        {
+            Solution sol = new Solution();
+            Assert.AreEqual(sol.NumDecodings("123"), 3);
+        }
+
+        [TestMethod()]
+        public void NumDecodingsTest3()
+        {
+            Solution sol = new Solution();
+            Assert.AreEqual(sol.NumDecodings("10"), 1);
+        }
+
+        [TestMethod()]
+        public void NumDecodingsTest4()
+        {
+            Solution sol = new Solution();
+            Assert.AreEqual(sol.NumDecodings("230"), 0);
+        }
+
+        [TestMethod()]
+        public void NumDecodingsTest5()
+        {
+            Solution sol = new Solution();
+            Assert.AreEqual(sol.NumDecodings("17"), 2);
+        }
+
+        [TestMethod()]
+        public void NumDecodingsTest6()
+        {
+            Solution sol = new Solution();
+            Assert.AreEqual(sol.NumDecodings("19963"), 2);
+        }
+
+        [TestMethod()]
+        public void NumDecodingsTest7()
+        {
+            Solution sol = new Solution();
+            Assert.AreEqual(sol.NumDecodings("9371597631128776948387197132267188677349946742344217846154932859125134924241649584251978418763151253"), 3981312);
         }
     }
 }
