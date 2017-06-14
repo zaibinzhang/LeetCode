90f19285149db5012958b8f21aad481593c19dea
zaibinzhang <zaibinzhang@qq.com>
2017-06-14 16:30:31 +0800

    出错

--- LeetCode/Algorithm/ZigzagLevelOrder.cs
+++ LeetCode/Algorithm/ZigzagLevelOrder.cs
@@ -8,7 +8,31 @@ namespace LeetCode.Algorithm
         //103. Binary Tree Zigzag Level Order Traversal
         public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
         {
-            return null;
+            return ZigzagLevelOrder(new List<TreeNode>() { root });
+        }
+
+        private IList<IList<int>> ZigzagLevelOrder(List<TreeNode> nodes)
+        {
+            if (nodes.Count == 0)
+            {
+                return new List<IList<int>>();
+            }
+            List<IList<int>> rList = new List<IList<int>>();
+            IList<int> list = new List<int>();
+            List<TreeNode> nList = new List<TreeNode>();
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                var n = nodes.Count - i - 1;
+                list.Add(nodes[n].val);
+                nList.Add(nodes[n]);
+            }
+            rList.Add(list);
+            var subList = ZigzagLevelOrder(nList);
+            if (subList.Count != 0)
+            {
+                rList.AddRange(subList);
+            }
+            return rList;
         }
     }
 }
