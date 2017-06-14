15994777b3140df34438d5bfb47b94c77d20b9a6
zaibinzhang <zaibinzhang@qq.com>
2017-06-14 14:55:53 +0800

    完成

--- LeetCode/Algorithm/RestoreIpAddresses.cs
+++ LeetCode/Algorithm/RestoreIpAddresses.cs
@@ -7,7 +7,43 @@ namespace LeetCode.Algorithm
         //93. Restore IP Addresses
         public IList<string> RestoreIpAddresses(string s)
         {
-            return new List<string>() { "255.255.11.135", "255.255.111.35" };
+            return RestoreIpAddresses(s, 4);
+        }
+
+        private IList<string> RestoreIpAddresses(string s, int num)
+        {
+            if (string.IsNullOrEmpty(s) || s.Length < num || s.Length > num * 3)
+            {
+                return new List<string>();
+            }
+            if (num == 1)
+            {
+                if (int.Parse(s) > 255 || (s.Length > 1 && s[0] == '0'))
+                {
+                    return new List<string>();
+                }
+                return new List<string>() { s };
+            }
+            IList<string> list = new List<string>();
+            for (int i = 1; i < s.Length + 1; i++)
+            {
+                string start = s.Substring(0, i);
+                if (int.Parse(start) > 255 || (i > 1 && start[0] == '0'))
+                {
+                    break;
+                }
+                var subList = RestoreIpAddresses(s.Substring(i), num - 1);
+                if (subList == null || subList.Count == 0)
+                {
+                    continue;
+                }
+                foreach (string sb in subList)
+                {
+                    list.Add(start + "." + sb);
+                }
+
+            }
+            return list;
         }
     }
 }
--- LeetCodeTests/Algorithm/RestoreIpAddressesTests.cs
+++ LeetCodeTests/Algorithm/RestoreIpAddressesTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using LeetCode.Algorithm;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -6,19 +9,28 @@ namespace LeetCodeTests.Algorithm
 {
     public partial class SolutionTests
     {
-        private bool CompareList<T>(IList<T> thisList, IList<T> thatList)
+        private bool CompareList<T>(IList<T> thisList, IList<T> thatList) where T : IComparable
         {
-            foreach (T t in thatList)
+            if (thisList == thatList)
             {
-                if (!thisList.Contains(t))
+                return true;
+            }
+            if (thisList == null || thatList == null)
+            {
+                return false;
+            }
+            if (thisList.Count != thatList.Count)
+            {
+                return false;
+            }
+            var ti = thisList.OrderBy(t => t).ToArray();
+            var ta = thatList.OrderBy(t => t).ToArray();
+            for (int i = 0; i < ti.Length; i++)
+            {
+                if (ti[i].CompareTo(ta[i]) != 0)
                 {
                     return false;
                 }
-                thisList.Remove(t);
-            }
-            if (thisList.Count>0)
-            {
-                return false;
             }
             return true;
         }
@@ -28,6 +40,13 @@ namespace LeetCodeTests.Algorithm
         {
             Solution sol = new Solution();
             Assert.IsTrue(CompareList(sol.RestoreIpAddresses("25525511135"), new List<string>() { "255.255.11.135", "255.255.111.35" }));
+        }
+
+        [TestMethod()]
+        public void RestoreIpAddressesTest2()
+        {
+            Solution sol = new Solution();
+            Assert.IsTrue(CompareList(sol.RestoreIpAddresses("010010"), new List<string>() { "0.10.0.10", "0.100.1.0" }));
         }
     }
 }
