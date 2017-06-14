d947bead10a313ed0463dda7c443f34f227eb782
zaibinzhang <zaibinzhang@qq.com>
2017-06-14 14:59:38 +0800

    95. Unique Binary Search Trees II

---
+++ LeetCode/Algorithm/GenerateTrees.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using LeetCode.Model;
+
+namespace LeetCode.Algorithm
+{
+    //95. Unique Binary Search Trees II
+    public partial class Solution
+    {
+        public IList<TreeNode> GenerateTrees(int n)
+        {
+            return null;
+        }
+    }
+}
