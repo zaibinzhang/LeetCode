f999f6334c79a2bcefa6ae7413117f2f91f526e2
zaibinzhang <zaibinzhang@qq.com>
2017-06-14 16:14:08 +0800

    过！

--- LeetCode/Algorithm/IsValidBST.cs
+++ LeetCode/Algorithm/IsValidBST.cs
@@ -8,7 +8,36 @@ namespace LeetCode.Algorithm
     {
         public bool IsValidBST(TreeNode root)
         {
-            return false;
+            if (root == null)
+            {
+                return true;
+            }
+            if (root.left != null)
+            {
+                var left = root.left;
+                while (left.right != null)
+                {
+                    left = left.right;
+                }
+                if (left.val >= root.val)
+                {
+                    return false;
+                }
+            }
+            if (root.right != null)
+            {
+                var right = root.right;
+                while (right.left != null)
+                {
+                    right = right.left;
+                }
+                if (right.val <= root.val)
+                {
+                    return false;
+                }
+            }
+            return IsValidBST(root.left) && IsValidBST(root.right);
         }
+
     }
 }
