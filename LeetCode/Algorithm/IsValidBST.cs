b26073358f4d999e111c7e8d74fc9cb907f229de
zaibinzhang <zaibinzhang@qq.com>
2017-06-14 15:49:39 +0800

    98. Validate Binary Search Tree

---
+++ LeetCode/Algorithm/IsValidBST.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Model;
+// ReSharper disable All
+
+namespace LeetCode.Algorithm
+{
+    //98. Validate Binary Search Tree
+    public partial class Solution
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            return false;
+        }
+    }
+}
