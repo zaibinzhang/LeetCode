80639b95062fc9492446cf65d47e78a3271162d1
zaibinzhang <zaibinzhang@qq.com>
2017-06-15 10:21:56 +0800

    105. Construct Binary Tree from Preorder and Inorder Traversal

---
+++ LeetCode/Algorithm/BuildTree.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Model;
+
+namespace LeetCode.Algorithm
+{
+    public partial class Solution
+    {
+        //105. Construct Binary Tree from Preorder and Inorder Traversal
+        public TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            return null;
+        }
+    }
+}
