23b5d22e8edd0b9b5dc6cb1d89ddac10653569c2
zaibinzhang <zaibinzhang@qq.com>
2017-06-15 15:45:45 +0800

    106. Construct Binary Tree from Inorder and Postorder Traversal

---
+++ LeetCode/Algorithm/BuildTree2.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Model;
+
+namespace LeetCode.Algorithm
+{
+    public partial class Solution
+    {
+        //106. Construct Binary Tree from Inorder and Postorder Traversal
+        public TreeNode BuildTree2(int[] inorder, int[] postorder)
+        {
+            return null;
+        }
+    }
+}
