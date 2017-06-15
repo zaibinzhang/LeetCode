c85e993a3958ed3307121027853cbb5894e1f3e3
zaibinzhang <zaibinzhang@qq.com>
2017-06-15 16:47:22 +0800

    初步完成

--- LeetCode/Algorithm/BuildTree2.cs
+++ LeetCode/Algorithm/BuildTree2.cs
@@ -7,7 +7,36 @@ namespace LeetCode.Algorithm
         //106. Construct Binary Tree from Inorder and Postorder Traversal
         public TreeNode BuildTree2(int[] inorder, int[] postorder)
         {
-            return null;
+            return BuildTree2(0, postorder.Length - 1, postorder.Length - 1, inorder, postorder);
+        }
+
+        public TreeNode BuildTree2(int start, int end, int index, int[] inorder, int[] postorder)
+        {
+            if (start > end)
+            {
+                return null;
+            }
+            if (start == end)
+            {
+                return new TreeNode(inorder[start]);
+            }
+            int val = postorder[index];
+            TreeNode node = new TreeNode(val);
+            int valIndex = 0;
+            for (int i = start; i <= end; i++)
+            {
+                if (inorder[i] == val)
+                {
+                    valIndex = i;
+                    break;
+                }
+            }
+            if (end > valIndex)
+            {
+                node.right = BuildTree2(valIndex + 1, end, --index, inorder, postorder);
+            }
+            node.left = BuildTree2(start, valIndex - 1, index - 1, inorder, postorder);
+            return node;
         }
     }
 }
