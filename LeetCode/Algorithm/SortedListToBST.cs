87da57bdbd066cf2aafc65d86e86c4a2980c5279
zaibinzhang <zaibinzhang@qq.com>
2017-06-16 11:40:32 +0800

    109. Convert Sorted List to Binary Search Tree

---
+++ LeetCode/Algorithm/SortedListToBST.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Model;
+
+namespace LeetCode.Algorithm
+{
+    public partial class Solution
+    {
+        //109. Convert Sorted List to Binary Search Tree
+        // ReSharper disable once InconsistentNaming
+        public TreeNode SortedListToBST(ListNode head)
+        {
+            return null;
+        }
+    }
+}
